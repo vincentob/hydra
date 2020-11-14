21e17cced9837dbc36087f1daf7a61bea6c4ea2a
vincent <sslnwwx@163.com>
2020-11-14 23:51:07 +0800

    add formutil.authorized.go

--- formutil/authorized.go
+++ formutil/authorized.go
@@ -3,12 +3,19 @@ package formutil
 import (
 	"net/http"
 
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 )
 
-func Authorized(c *gin.Context) {
-	if _, ok := GetSessionID(c); !ok {
-		c.JSON(http.StatusUnauthorized, "Unauthorized")
-		c.Abort()
+const (
+	DefaultUserKey = "username"
+)
+
+// Authorization check user session
+func Authorization() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if sessions.Default(c).Get(DefaultUserKey) == nil {
+			c.AbortWithStatus(http.StatusUnauthorized)
+		}
 	}
 }

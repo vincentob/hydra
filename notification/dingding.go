f563d39d20610c00f09469bb56a1c65d35407d90
vincent <sslnwwx@163.com>
2021-02-14 20:44:36 +0800

    add notification module

---
+++ notification/dingding.go
@@ -0,0 +1,64 @@
+package notification
+
+import (
+	"github.com/dantin-s/hydra/httpclient"
+	"github.com/dantin-s/hydra/json"
+	"github.com/pkg/errors"
+)
+
+const (
+	MsgTypeLink = "link"
+	MsgTypeText = "text"
+	MsgTypeMD   = "markdown"
+)
+
+// DDMsg dingding 告警消息体，默认是 Link 类型
+type DDMsg struct {
+	Msgtype  string        `json:"msgtype"`
+	Link     DDMsgLink     `json:"link,omitempty"`
+	Text     DDMsgText     `json:"text,omitempty"`
+	Markdown DDMsgMarkDown `json:"markdown,omitempty"`
+	At       DDAt          `json:"at,omitempty"`
+}
+
+// DDMsgLink link 类型的 msg
+type DDMsgLink struct {
+	Text       string `json:"text"`
+	Title      string `json:"title"`
+	PicURL     string `json:"picUrl"`
+	MessageURL string `json:"messageUrl"`
+}
+
+type DDMsgText struct {
+	Content string `json:"content"`
+}
+
+type DDMsgMarkDown struct {
+	Title string `json:"title"`
+	Text  string `json:"text"`
+}
+
+type DDAt struct {
+	AtMobiles []string `json:"atMobiles,omitempty"`
+	IsAtAll   bool     `json:"isAtAll,omitempty"`
+}
+
+type DDResponse struct {
+	Errcode int64  `json:"errcode,omitempty"`
+	Errmsg  string `json:"errmsg,omitempty"`
+}
+
+func SendDD(msg *DDMsg, url string) (*DDResponse, error) {
+	data, err := json.FastJJ.Marshal(msg)
+	if err != nil {
+		return nil, err
+	}
+
+	var dingDingResp DDResponse
+	_, err = httpclient.DoPostJson(url, nil, data, &dingDingResp)
+	if err != nil {
+		return nil, errors.Wrap(err, "SendDD failed")
+	}
+
+	return &dingDingResp, nil
+}
---
+++ notification/weixin.go
@@ -0,0 +1,76 @@
+package notification
+
+import (
+	"fmt"
+
+	"github.com/dantin-s/hydra/httpclient"
+	"github.com/pkg/errors"
+)
+
+const (
+	WXTokenUrl = "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=%s&corpsecret=%s"
+	WXMsgUrl   = "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=%s"
+)
+
+// WXAccessTokenBody response body for get access token
+type WXAccessTokenBody struct {
+	ErrCode     int    `json:"errcode"`
+	ErrMsg      string `json:"errmsg"`
+	AccessToken string `json:"access_token"`
+	ExpiresIn   int    `json:"expires_in"`
+}
+
+// WXMsgBody the request body we send to weixin api gateway
+type WXMsgBody struct {
+	ToTag   string    `json:"totag"`
+	MsgType string    `json:"msgtype"`
+	AgentId string    `json:"agentid"`
+	Text    WXMsgText `json:"text"`
+	News    WXMsgNews `json:"news"`
+}
+
+// WXMsgText for wx text body
+type WXMsgText struct {
+	Content string `json:"content"`
+}
+
+// WXMsgNews for wx news type body
+type WXMsgNews struct {
+	Articles []WXMsgArticle `json:"articles"`
+}
+
+// WXMsgArticle for article type body
+type WXMsgArticle struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	URL         string `json:"url"`
+	PicURL      string `json:"picurl"`
+}
+
+type WXResponse struct {
+	ErrCode     int    `json:"errcode"`
+	ErrMsg      string `json:"errmsg"`
+	Invaliduser string `json:"invaliduser"`
+}
+
+// SendWX
+func SendWX(content *WXMsgBody, agentID string, agentSecret string) error {
+	// get accesstoken first
+	accessToken := &WXAccessTokenBody{}
+	tokenURL := fmt.Sprintf(WXTokenUrl, agentID, agentSecret)
+	if _, err := httpclient.DoGetJson(tokenURL, nil, accessToken); err != nil {
+		return errors.Wrap(err, "get accesstoken failed")
+	}
+
+	if accessToken.ErrCode != 0 {
+		return errors.Wrap(errors.New(accessToken.ErrMsg), "get accessToken failed")
+	}
+
+	msgURL := fmt.Sprintf(WXMsgUrl, accessToken)
+	wxResult := &WXResponse{}
+	if _, err := httpclient.DoPostJson(msgURL, nil, content, wxResult); err != nil {
+		return errors.Wrap(errors.New(wxResult.ErrMsg), "send wx failed")
+	}
+
+	return nil
+}
